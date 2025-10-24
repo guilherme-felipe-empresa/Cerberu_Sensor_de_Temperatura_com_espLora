9e75dc068d9e7e4611af221be07c5fb061a40217
Guilherme Felipe <80486962+guilherme-felipe-empresa@users.noreply.github.com>
2025-10-24 17:54:00 -0300

    Refactor OLED_LoRa_Receiver setup and clean code
    Removed commented-out code and simplified Heltec.begin parameters.

--- OLED_LoRa_Receiver/OLED_LoRa_Receiver.ino
+++ OLED_LoRa_Receiver/OLED_LoRa_Receiver.ino
@@ -1,14 +1,14 @@
 #include "heltec.h" 
-//#include "images.h"
 
-#define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
+
+#define BAND    915E6
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
 
 void logo(){
   Heltec.display->clear();
- // Heltec.display->drawXbm(0,5,logo_width,logo_height,logo_bits);
+
   Heltec.display->display();
 }
 
@@ -16,7 +16,7 @@ void LoRaData(){
   Heltec.display->clear();
   Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
   Heltec.display->setFont(ArialMT_Plain_10);
-  //Heltec.display->drawString(0 , 15 , "Received "+ packSize + " bytes");
+
   Heltec.display->drawStringMaxWidth(0 , 26 , 128, packet);
   Heltec.display->drawString(0, 0, rssi);  
   Heltec.display->display();
@@ -38,8 +38,8 @@ void msgDisplay(String frase){
   }
 
 void setupReceiverLora() { 
-   //WIFI Kit series V1 not support Vext control
-  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+  
+  Heltec.begin(true, true, true, true, BAND);
  
   Heltec.display->init();
   Heltec.display->flipScreenVertically();  
@@ -49,7 +49,7 @@ void setupReceiverLora() {
 
   Heltec.display->clear();
   
-Heltec.display->drawString(25, 0, "Ambev");
+  Heltec.display->drawString(25, 0, "Ambev");
   Heltec.display->setFont(ArialMT_Plain_16);
   
   
@@ -58,17 +58,5 @@ Heltec.display->drawString(25, 0, "Ambev");
   Heltec.display->display();
   delay(1500);
   Heltec.display->clear();
-//  Heltec.display->drawString(0, 0, "Connected: "+ String(IP));
-//  delay(1000);
-  //LoRa.onReceive(cbk);
   LoRa.receive();
 }
-
-//void LoraReceiver() {
-//  int packetSize = LoRa.parsePacket();
-//  if (packetSize) { 
-//    cbk(packetSize);  
-//    Serial.println(packet);
-//  }
-//  delay(10);
-//}
