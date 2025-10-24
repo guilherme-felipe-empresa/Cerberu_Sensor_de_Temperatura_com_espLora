044d2f95d12a533d8a366f13f4415ad2ec3094ad
Guilherme Felipe <80486962+guilherme-felipe-empresa@users.noreply.github.com>
2025-10-24 17:50:53 -0300

    Update OLED_LoRa_Sender.ino

--- OLED_LoRa_Sender/OLED_LoRa_Sender.ino
+++ OLED_LoRa_Sender/OLED_LoRa_Sender.ino
@@ -1,5 +1,4 @@
 #include "heltec.h"
-//#include "images.h"
 
 #define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
 
@@ -8,18 +7,11 @@ String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
 
-//void logo()
-//{
-//  Heltec.display->clear();
-//  Heltec.display->drawXbm(0,5,logo_width,logo_height,logo_bits);
-//  Heltec.display->display();
-//}
 
 void setup()
 {
   
-   //WIFI Kit series V1 not support Vext control
-  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+  Heltec.begin(true, true, true, true, BAND);
  
   Heltec.display->init();
   Heltec.display->flipScreenVertically();  
@@ -57,13 +49,6 @@ void loop()
   // send packet
   LoRa.beginPacket();
   
-/*
- * LoRa.setTxPower(txPower,RFOUT_pin);
- * txPower -- 0 ~ 20
- * RFOUT_pin could be RF_PACONFIG_PASELECT_PABOOST or RF_PACONFIG_PASELECT_RFO
- *   - RF_PACONFIG_PASELECT_PABOOST -- LoRa single output via PABOOST, maximum output 20dBm
- *   - RF_PACONFIG_PASELECT_RFO     -- LoRa single output via RFO_HF / RFO_LF, maximum output 14dBm
-*/
   LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
   LoRa.print("Celsius: ");
   LoRa.print(Temperatura());
@@ -71,10 +56,4 @@ void loop()
   LoRa.endPacket();
 
   lcd_lora = "";
-
- 
-//  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);                       // wait for a second
-//  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-//  delay(1000);                       // wait for a second
 }
